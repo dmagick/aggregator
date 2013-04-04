054380a3dc380bdd66e721188c956c0b7e2cc052
Chris Smith <dmagick@gmail.com>
2013-04-04 15:39:19 +1100

    some tables to start storing things in. "feeds" is just for the feed url (rss/rdf url). urls is for the urls inside the feed. users_feeds will record when a particular user looks at a particular feed (so feeds can be hit once for multiple users, rather than once per user), and users_urls will do the same for urls.

--- db/create.sql
+++ db/create.sql
@@ -1,3 +1,8 @@
+drop table aggregator_users_feeds;
+drop table aggregator_users_urls;
+drop table aggregator_urls;
+drop table aggregator_feeds;
+
 drop table aggregator_users;
 drop table aggregator_user_login_locks;
 
@@ -12,9 +17,37 @@ create index aggregator_user_login_locks_details on aggregator_user_login_locks(
 
 create table aggregator_users
 (
-  user_id serial not null primary key,
-  username text unique not null,
+  username text not null primary key,
   passwd text not null,
   useractive char(1) default 'n'
+);
+
+create table aggregator_feeds
+(
+	feed_url text not null primary key,
+	last_checked timestamp,
+	last_status int
+);
+
+create table aggregator_urls
+(
+	url text not null primary key,
+	feed_url text references aggregator_feeds(feed_url) on delete cascade,
+	last_checked timestamp,
+	status int
+);
+
+create table aggregator_users_feeds
+(
+	username text not null references aggregator_users(username) on delete cascade,
+	feed_url text not null references aggregator_feeds(feed_url) on delete cascade,
+	user_checked timestamp
+);
+
+create table aggregator_users_urls
+(
+	username text not null references aggregator_users(username) on delete cascade,
+	url text not null references aggregator_urls(url) on delete cascade,
+	user_checked timestamp
 );
 
